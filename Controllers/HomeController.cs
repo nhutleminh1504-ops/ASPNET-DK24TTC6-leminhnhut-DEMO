aeeba6baeeaec3a2b1b282c184de29ef295587b1
Le-Minh <nhutleminh1504@gmail.com>
2026-04-18 20:20:01 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using DoAnHMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnHMS.Controllers
+{
+    public class HomeController : BaseController
+    {
+        doanKSEntities db = new doanKSEntities();
+        public ActionResult Index()
+        {
+            List<string> maPDP = db.PhieuDatPhongs.Select(x => x.maPDP).ToList();
+            List<string> maPDPinPTP = db.PhieuThuePhongs.Select(x => x.maPDP).ToList();
+            for (int i = 0; i < maPDPinPTP.Count; i++)
+            {
+                string item = maPDPinPTP[i];
+                if (maPDP.Contains(item))
+                {
+                    maPDP.Remove(item);
+                }
+            }
+            //var phieuDatPhongs = db.PhieuDatPhongs
+            var lienHe = db.LienHes.Where(x => x.tinhTrang == false).ToList();
+            var phanHoi = db.PhanHois.Where(x => x.tinhTrang == false).ToList();
+            ViewBag.PDP = maPDP.Count() == 0 ? "" : maPDP.Count().ToString();
+            ViewBag.LienHe = lienHe.Count() == 0 ? "" : lienHe.Count().ToString();
+            ViewBag.PhanHoi = phanHoi.Count() == 0 ? "" : phanHoi.Count().ToString();
+            return View();
+        }
+    }
+}
